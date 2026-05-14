8fb15b467a75a488d5a52c22ba29c59aef8a832d
Chao <114423476+ChaoIsBalling@users.noreply.github.com>
2026-05-14 18:08:53 +0200

    PACO

---
+++ Scripts/paco.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class paco : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        string paco = "paco";
+        TMP_Text texto = GetComponent<TMP_Text>();
+        Convert conversion = devuelve;
+        texto.text= conversion(paco);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    delegate string Convert(string f);
+    private static string devuelve(string f)
+    {
+        return f + "salchichon";
+    }
+}
